7be519d9a75db51ecfe28a7c8d2d77b6953470ff
Ioann Liubchenko <liubchenko18@gmail.com>
2024-12-26 01:52:24 +0200

    Test Prettier integration

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'no-unused-vars': ['warn'],
+    },
+  },
+];
